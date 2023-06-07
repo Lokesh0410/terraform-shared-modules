4cc669066e0269f199422b115ad814d7bfb769a4
Lokesh Arora <lokesharora@Lokeshs-MacBook-Air.local>
2023-06-07 18:22:18 +0100

    Added vpc module

--- aws_ec2_instance/ec2.tf
+++ aws_ec2_instance/ec2.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "this" {
-  ami = "ami-09e2d756e7d78558d"
-  instance_type = "t2.micro"
-  availability_zone = "ap-south-1"
-  key_name = "lokesh-test"
-  disable_api_termination = "true"
-  vpc_security_group_ids = [ aws_security_group.security_group.id ]
-}
+# resource "aws_instance" "this" {
+#   ami = "ami-09e2d756e7d78558df"
+#   instance_type = "t2.micro"
+#   availability_zone = "ap-south-1"
+#   key_name = "lokesh-test"
+#   disable_api_termination = "true"
+#   vpc_security_group_ids = [ aws_security_group.security_group.id ]
+# }
